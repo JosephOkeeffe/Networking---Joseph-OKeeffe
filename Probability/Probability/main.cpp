108022c8d5b5976979bcea5756a5362915fd1271
JosephOkeeffe <josephdokeeffe@yahoo.ie>
2023-10-03 16:30:50 +0100

    Done lab 2

--- Probability/Probability/main.cpp
+++ Probability/Probability/main.cpp
@@ -9,17 +9,17 @@ void main()
 {
 	//int factorial = Factorial(6);
 	//float answer = N_Choose_K(6, 0);
-	float a;
-	for (int i = 0; i < 500; i++)
+	for (int i = 2; i < 16; i++)
 	{
-		a = ToGetProbability(0.1, 0.9, i, 2);
+		 ToGetProbability(0.1, 0.9, i, 2);
 
-		if (a >= 40)
-		{
-			std::cout << i << "\n";
-		}
+
 	}
 	
+	 //ToGetProbability(0.1, 0.9, 6, 1);
+	 //ToGetProbability(0.1, 0.9, 6, 2);
+	 //ToGetProbability(0.1, 0.9, 6, 3);
+
 
 }
 
@@ -41,7 +41,7 @@ float Factorial(float n)
 		}
 		
 	}
-	//std::cout << "Factorial of ' " << start << " ' = " << result << "\n";
+	std::cout << "Factorial of ' " << start << " ' = " << result << "\n";
 	return result;
 	
 }
@@ -59,21 +59,21 @@ float ToGetProbability(float p, float q, float n, float k)
 {
 	float result;
 
-	//std::cout << "Probability of success = p: " <<p<< "\n";
-	//std::cout << "Probability of failure = q: " <<q<< "\n";
-	//std::cout << "Total number of trials = n: " <<n<< "\n";
-	//std::cout << "Number of successful trials = k: " <<k<< "\n";
-	//std::cout << "------------------------------------------------\n";
+	std::cout << "Probability of success = p: " <<p<< "\n";
+	std::cout << "Probability of failure = q: " <<q<< "\n";
+	std::cout << "Total number of trials = n: " <<n<< "\n";
+	std::cout << "Number of successful trials = k: " <<k<< "\n";
+	std::cout << "------------------------------------------------\n";
 
 	float part1 = N_Choose_K(n, k);
-	//std::cout << "C(n,k) = C("<< n <<","<< k <<") = "  << part1 << " \n";
+	std::cout << "C(n,k) = C("<< n <<","<< k <<") = "  << part1 << " \n";
 	float part2 = std::pow(p, k);
-	//std::cout << "(p^k) = (" << p << "^" << k << ") = " << part2 << " \n";
+	std::cout << "(p^k) = (" << p << "^" << k << ") = " << part2 << " \n";
 	float part3 = std::pow(q, (n - k));
-	//std::cout << "(q^(n-k)) = (" << q << "^(" << n << "-" << k << ") = " << part3 << " \n";
+	std::cout << "(q^(n-k)) = (" << q << "^(" << n << "-" << k << ") = " << part3 << " \n";
 	result = (part1 * (part2) * part3);
 	std::cout << part1 << " * " << part2 << " * " << part3 << " = " << result << "\n";
-	//std::cout << "------------------------------------------------\n";
+	std::cout << "------------------------------------------------\n";
 	result *= 100;
 
 	std::cout << "Result: " << result << "% \n";
